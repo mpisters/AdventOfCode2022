8363dcfdb158107a852832246ff913fecd60067e
Michelle <infi-gvb@MacBook-Pro-van-INFI-gvb.local>
2022-12-27 14:54:53 +0100

    day 6 part 2

--- AdventOfCode2022.Tests/Day6Test.cs
+++ AdventOfCode2022.Tests/Day6Test.cs
@@ -8,13 +8,12 @@ public class Day6Test
    
     [Theory]
     [InlineData("../../../../AdventOfCode2022/Day6/day6inputsubsetpart1.txt", new int[] {7,5,6, 10,11})]
-    // [InlineData("../../../../AdventOfCode2022/Day6/day6inputpart1.txt", 69206)]
     public void TestPart1(string pathToInput, int[] expectedIncreasedTotal)
     {
         var filehelper = new FileReaderHelper();
         var input = filehelper.GetStringsOfFile(pathToInput);
         var day6 = new Day6();
-        var solution = day6.GetPart1(input);
+        var solution = day6.GetPart1(input, 4);
         Assert.Equal(expectedIncreasedTotal[0], solution[0]);
         Assert.Equal(expectedIncreasedTotal[1], solution[1]);
         Assert.Equal(expectedIncreasedTotal[2], solution[2]);
@@ -29,23 +28,33 @@ public class Day6Test
         var filehelper = new FileReaderHelper();
         var input = filehelper.GetStringsOfFile(pathToInput);
         var day6 = new Day6();
-        var solution = day6.GetPart1(input);
+        var solution = day6.GetPart1(input, 4);
         Assert.Equal(expectedIncreasedTotal[0], solution[0]);
     }
         
     [Theory]
     [InlineData("../../../../AdventOfCode2022/Day6/day6inputsubsetpart1.txt", new int[] {19,23,23, 29,26})]
-    // [InlineData("../../../../AdventOfCode2022/Day6/day6inputpart1.txt", 197400)]
-    public void TestPart(string pathToInput, int[] expectedIncreasedTotal)
+    public void TestPart2(string pathToInput, int[] expectedIncreasedTotal)
     {
         var filehelper = new FileReaderHelper();
         var input = filehelper.GetStringsOfFile(pathToInput);
         var day6 = new Day6();
-        var solution = day6.GetPart2(input);
+        var solution = day6.GetPart2(input, 14);
         Assert.Equal(expectedIncreasedTotal[0], solution[0]);
         Assert.Equal(expectedIncreasedTotal[1], solution[1]);
         Assert.Equal(expectedIncreasedTotal[2], solution[2]);
         Assert.Equal(expectedIncreasedTotal[3], solution[3]);
         Assert.Equal(expectedIncreasedTotal[4], solution[4]);
+    }
+    
+    [Theory]
+    [InlineData("../../../../AdventOfCode2022/Day6/day6inputpart1.txt", new int[] {2508})]
+    public void TestPart2RealInput(string pathToInput, int[] expectedIncreasedTotal)
+    {
+        var filehelper = new FileReaderHelper();
+        var input = filehelper.GetStringsOfFile(pathToInput);
+        var day6 = new Day6();
+        var solution = day6.GetPart2(input, 14);
+        Assert.Equal(expectedIncreasedTotal[0], solution[0]);
     }
 }
--- AdventOfCode2022/Day6/Day6.cs
+++ AdventOfCode2022/Day6/Day6.cs
@@ -3,37 +3,36 @@ namespace AdventOfCode2022.Day6;
 public class Day6
 
 {
-    public List<int> GetPart1(List<string> inputValues)
+    public List<int> GetPart1(List<string> inputValues, int totalDistinctValues)
     {
-        return inputValues.Select(GetFirstMarker).ToList();
+        return inputValues.Select(row => GetFirstMarker(row, totalDistinctValues)).ToList();
     }
 
-    private int GetFirstMarker(string signal)
+    private int GetFirstMarker(string signal, int totalDistinctValues)
     {
         var score = int.MaxValue;
-        for (var i = 0; i < signal.Count() - 3; i++)
+        for (var i = 0; i < signal.Count() - (totalDistinctValues - 1); i++)
         {
-            var lengthOfSubpart = 4;
-            var substring = signal.Substring(i, lengthOfSubpart);
+            var substring = signal.Substring(i, totalDistinctValues);
             var uniqueCharacters = new HashSet<char>();
             foreach (var character in substring)
             {
                 uniqueCharacters.Add(character);
             }
 
-            if (uniqueCharacters.Count != lengthOfSubpart)
+            if (uniqueCharacters.Count != totalDistinctValues)
             {
                 continue;
             };
-            score = i + lengthOfSubpart;
+            score = i + totalDistinctValues;
             break;
         }
 
         return score;
     }
 
-    public List<int> GetPart2(List<string> inputValues)
+    public List<int> GetPart2(List<string> inputValues, int totalDistinctValues)
     {
-        throw new NotImplementedException();
+        return inputValues.Select(row => GetFirstMarker(row, totalDistinctValues)).ToList();
     }
 }
